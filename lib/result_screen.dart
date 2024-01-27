9b2bd3a8bdadda4dfc70944ac0f5407e9be136fa
Cristian Centeno <centenosx@gmail.com>
2024-01-27 08:11:37 -0500

    Primer commit: Proyecto ganga azar una app que elije un biker de manera aletaroia a una persona

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ResultScreen extends StatelessWidget {
   final String selectedBiker;
@@ -9,16 +10,37 @@ class ResultScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Resultado'),
+        title: Text(
+          'Resultado',
+          style: GoogleFonts
+              .playfairDisplay(), // Cambia 'honk' por el nombre de la fuente que deseas utilizar
+        ),
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.red,
       ),
-      body: Center(
-        child: Text(
-          'El elegido es:\n$selectedBiker',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/fondo1.jpg'),
+            fit: BoxFit.cover,
           ),
-          textAlign: TextAlign.center,
+        ),
+        child: Center(
+          child: Container(
+            color: Colors.black.withOpacity(0.8),
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'La victima de hoy fue:\n$selectedBiker',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.red,
+                fontFamily: GoogleFonts.playfairDisplay()
+                    .fontFamily, // Asegúrate de usar la misma fuente aquí
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
         ),
       ),
     );
